53d177037e9b635c8834ca8302e0aae997f59d4a
Eden <pkh_eden@photons.com.tw>
2025-07-30 14:43:45 +0800

    Add AFNetworking wrapper for SPM

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "AFNetworkingWrapper",
+    platforms: [
+        .iOS(.v11), .macOS(.v10_13)
+    ],
+    products: [
+        .library(
+            name: "AFNetworking",
+            targets: ["AFNetworking"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "AFNetworking",
+            path: "Sources/AFNetworking/AFNetworking",
+            publicHeadersPath: ".",
+            cSettings: [
+                .headerSearchPath("."),
+                .define("AF_APP_EXTENSIONS")
+            ]
+        )
+    ]
+)
